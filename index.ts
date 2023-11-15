67094b7ecad855d35dce4d1d76eeeff79d72d586
Jordi <jllahi@users.noreply.github.com>
2023-11-15 13:27:54 +0100

    Add first version

---
+++ index.ts
@@ -0,0 +1,7 @@
+// Do not write code directly here, instead use the `src` folder!
+// Then, use this file to export everything you want your user to access.
+
+// @ts-expect-error
+import TailwindIndicator from './src/TailwindIndicator.astro'
+
+export default TailwindIndicator
